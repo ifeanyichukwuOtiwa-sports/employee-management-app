5c7c8d5d76f022ce141f49e2545150f62196c281
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2023-08-02 20:42:56 +0100

    ** remove unnecessary lombok annotations

--- employee-management-b-e/app/persistence/src/main/java/org/gxstar/employee/persistence/model/Employee.java
+++ employee-management-b-e/app/persistence/src/main/java/org/gxstar/employee/persistence/model/Employee.java
@@ -3,16 +3,12 @@ package org.gxstar.employee.persistence.model;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @Setter
-@ToString
 @EqualsAndHashCode
-@NoArgsConstructor
 public class Employee {
 
     private Long id;
